be91789459fd15c74ac8e47a81d42f2515cdd9bd
Jeremy Schroeder <jeremy@ssdbg.com>
2015-03-05 14:32:28 -0500

    Allow views to grab documents inline if 'include_docs=true' in the parameters.

--- src/main/java/net/aufdemrand/webizen/database/View.groovy
+++ src/main/java/net/aufdemrand/webizen/database/View.groovy
@@ -1,6 +1,9 @@
 package net.aufdemrand.webizen.database
 
+import groovy.json.JsonBuilder
 import org.codehaus.jackson.annotate.JsonIgnore
+import org.codehaus.jackson.map.DeserializationConfig
+import org.codehaus.jackson.map.ObjectMapper
 
 
 class View {
@@ -26,15 +29,51 @@ class View {
 
     public List<Document> getDocuments() {
         List<Document> docs = new ArrayList<Document>();
-        for (def entry in rows)
-            docs.add(Document.get(entry['id'], database))
+        for (def entry in rows) {
+            def returned = entry['doc']
+            if (returned == null) {
+                docs.add(CouchHandler.couch.getDoc(entry['id'], database, Document.class))
+            } else {
+                // ObjectMapper for Jackson Deserialization
+                ObjectMapper objectMapper = new ObjectMapper()
+                objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                // Fetch document from the return string
+                try {
+                    def doc = objectMapper.readValue(new JsonBuilder(returned).toPrettyString(), Document.class)
+                    if (doc._id == null) continue;
+                    else {
+                        docs.add(doc)
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace(); return null
+                }
+            }
+        }
         return docs;
     }
 
     public <T> T getAs(Class type) {
         def docs = new ArrayList<>();
-        for (def entry in rows)
-            docs.add(type.cast(CouchHandler.couch.getDoc(entry['id'], database, type)))
+        for (def entry in rows) {
+            def returned = entry['doc']
+            if (returned == null) {
+                docs.add(type.cast(CouchHandler.couch.getDoc(entry['id'], database, type)))
+            } else {
+                // ObjectMapper for Jackson Deserialization
+                ObjectMapper objectMapper = new ObjectMapper()
+                objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                // Fetch document from the return string
+                try {
+                    def doc = objectMapper.readValue(new JsonBuilder(returned).toPrettyString(), type)
+                    if (doc._id == null) continue;
+                    else {
+                        docs.add(doc)
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace(); return null
+                }
+            }
+        }
         return docs;
     }
 
